b60069b73150d036c0b22385f9b70d25fd7c8d25
Rehan Maulana <rehanmaul111@gmail.com>
2023-05-11 10:22:01 +0700

    fix redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import RootRouter from "./config/router";
 import { PersistGate } from "redux-persist/integration/react";
-import { persiststore } from "./config/redux/store";
+import store, { persiststore } from "./config/redux/store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-    <PersistGate loading={null} persistor={persiststore}>
-      <RootRouter />
-    </PersistGate>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persiststore}>
+        <RootRouter />
+      </PersistGate>
+    </Provider>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,10 @@ import App from "./App";
 import "./index.css";
 import { ApolloProvider } from "@apollo/client";
 import client from "./config/apollo/index";
-import { Provider } from "react-redux";
-import store from "./config/redux/store";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ApolloProvider client={client}>
     <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </React.StrictMode>
   </ApolloProvider>
 );
